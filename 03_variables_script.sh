9988451edf31d52562fe8fc1e05114b2478f9106
shiwangbisht <shiwangbisht2726@gmail.com>
2023-09-08 13:12:19 +0530

    variables in shell or bash scripting

---
+++ 03_variables_script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#variables are the box or container in which vslue is stored
+
+name=shiwang
+echo "hlo, my name is $name"
+
+# you can take same varaible with another value
+name=aman
+echo "hlo, my name is $name"
+
+#if you want to make a variable with permanant value
+readonly var_name=sillu
+echo $var_name
+
