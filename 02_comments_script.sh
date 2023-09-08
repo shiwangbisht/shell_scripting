764e926609e5e0179ced05e7edff7e13e3c3226c
shiwangbisht <shiwangbisht2726@gmail.com>
2023-09-08 13:11:45 +0530

    comments in shell or bash scripting

---
+++ 02_comments_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# this ia an single line comment
+echo "hello my life "
+
+<<comment
+using this
+we can perform 
+multiple line comments
+comment
+
+echo "comment's are very important "
+
